13358986b021d2af8a6e163b386696f44e4cd908
hb-33 <hb33@njit.edu>
2024-02-26 19:43:10 -0500

    Created breaches.controller.js

---
+++ backend/api/breaches.controller.js
@@ -0,0 +1,29 @@
+//Harshit Bansal, 3/1/24, IT302-002, Phase 2 Assignment: Read MongoDB data using Node.js, hb33@njit.edu
+
+import BreachesDAO from '../dao/breachesDAO.js'
+
+export default class BreachesController {
+  static async apiGetBreaches(req,res,next) {
+    const breachesPerPage = req.query.breachesPerPage ? parseInt(req.query.breachesPerPage) : 20
+    const page = req.query.page ? parseInt(req.query.page) : 0
+    let filters = {}
+    if(req.query.Domain){
+      filters.Domain = req.query.Domain
+    } else if(req.query.Name){
+        filters.Name = req.query.Name
+    } else if(req.query.BreachDate){
+        filters.BreachDate = req.query.BreachDate
+    }
+    const { breachesList, totalNumBreaches } = await BreachesDAO.getBreaches({
+    filters, page, breachesPerPage})
+
+    let response = {
+      breaches: breachesList,
+      page: page,
+      filters: filters,
+      entries_per_page: breachesPerPage,
+      total_results: totalNumBreaches,
+    }
+    res.json(response)
+   }
+}
