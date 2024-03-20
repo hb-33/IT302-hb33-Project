91a21615679be8e0b488744d244a5133e4fe5cc4
hb-33 <hb33@njit.edu>
2024-03-20 16:44:11 -0400

    Updated breaches.route.js

--- backend/api/breaches.route.js
+++ backend/api/breaches.route.js
@@ -1,10 +1,16 @@
-//Harshit Bansal, 3/1/24, IT302-002, Phase 2 Assignment: Read MongoDB data using Node.js, hb33@njit.edu
+//Harshit Bansal, 3/22/24, IT302-002, Phase 3 Assignment: C.U.D. MongoDB data using Node.js, hb33@njit.edu
 
 import express from 'express'
 import BreachesController from './breaches.controller.js'
+import AnalysesControlelr from './analyses.controller.js'
 
 const router = express.Router()
 
 router.route('/').get(BreachesController.apiGetBreaches)
+
+router.route('/analyses')
+    .post(AnalysesController.apiPostAnalyses)
+    .put(AnalysesController.apiUpdateReview)
+    .delete(AnalysesController.apiDeleteReview)
 
 export default router
