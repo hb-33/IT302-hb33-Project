d6e55933e4ec8280414e45b71c56a26c1137013d
hb-33 <hb33@njit.edu>
2024-03-20 17:04:34 -0400

    Created analysesDAO.js

---
+++ backend/dao/analysesDAO.js
@@ -0,0 +1,62 @@
+//Harshit Bansal, 3/22/24, IT302-002, Phase 3 Assignment: C.U.D. MongoDB data using Node.js, hb33@njit.edu
+
+import mongodb from "mongodb"
+const ObjectId = mongodb.ObjectId
+
+let analyses
+export default class AnalysesDAO {
+  static async injectDB(conn) {
+    if(analyses) {
+      return
+    } try {
+      analyses = await conn.db(process.env.BREACHANALYSES_NS).collection('analyses')
+    } catch(e) {
+      console.error(`unable to establish connection handle in analysesDAO: ${e}`)
+    }
+  }
+
+  static async addAnalysis(breachId, user, analysis, date) {
+    try {
+      const analysisDoc = {
+        name: user.name,
+        user_id: user._id,
+        date: date,
+        analysis: analysis,
+        breach_id: new ObjectId(breachId)
+      }
+      return await analyses.insertOne(analysisDoc)
+    } catch(e) {
+      console.error(`unable to post analysis: ${e}`)
+      console.error(e)
+      return { error: e }
+    }
+  }
+
+  static async updateAnalysis(analysisId, userId, analysis, date) {
+    try {
+      const updateResponse = await analyses.updateOne(
+        { user_id: userId, _id: new ObjectId(analysisId) },
+        { $set: { analysis: analysis, date: date } }
+      )
+      return updateResponse
+    } catch(e) {
+      console.error(`unable to update analysis: ${e}`)
+      console.error(e)
+      return { error: e}
+    }
+  }
+
+  static async deleteAnalysis(analysisId, userId) {
+    try {
+      const deleteResponse = await analyses.deleteOne({
+        _id: new ObjectId(analysisId),
+        user_id: userId,
+      })
+      return deleteResponse
+    } catch(e) {
+      console.error(`unable to delete analysis: ${e}`)
+      console.error(e)
+      return { error: e.message }
+    }
+  }
+}
