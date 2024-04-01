4f02b45925b49a963e463114ae4e3c364ed7d300
hb-33 <hb33@njit.edu>
2024-03-31 20:21:53 -0400

    Updated App.js/css

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,61 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from "react";
+import { Routes, Route, NavLink } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
 
+//import AddAnalysis from "./components/addAnalysis";
+import BreachesList from "./components/breachesList";
+import Breach from "./components/breach";
+import Login from "./components/login";
 
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
 function App() {
+  const [user, setUser] = useState(null);
+
+  async function login(user = null) {
+    setUser(user);
+  }
+
+  async function logout() {
+    setUser(null);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <div className="App">
+      <Navbar bg="light" expand="lg">
+        <Navbar.Brand>Data Breaches</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto">
+            <Nav.Link as={NavLink} to={"/breaches"}>
+              Breaches
+            </Nav.Link>
+            <Nav.Link as={NavLink} to={user ? "" : "/login"}>
+              {user ? "Logout User" : "Login"}
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+      <Routes>
+        <Route path="/" element={<BreachesList />}></Route>
+        <Route path="/breaches" element={<BreachesList />}></Route>
+
+        <Route path="/breaches/:id/" element={<Breach user={user} />}></Route>
+        <Route path="/login" element={<Login login={login} />}></Route>
+      </Routes>
     </div>
   );
 }
 
 export default App;
+
+/*
+ <Route
+          path="/hb33_breaches/:id/analysis"
+          element={<AddAnalysis user={user} />}
+        ></Route>
+
+        <Route path="/login" element={<Login login={login} />}></Route>
+
+*/
