6459644c6515040e57587d2be235b5be7ad870c9
hb-33 <hb33@njit.edu>
2024-04-03 18:24:08 -0400

    Created addAnalysis.js

---
+++ frontend/src/components/addAnalysis.js
@@ -0,0 +1,13 @@
+//Harshit Bansal, 4/12/24, IT302-002, Phase 4 Assignment: Read Node.js Data using React.js, hb33@njit.edu
+
+import React from "react";
+
+function AddAnalysis() {
+  return (
+    <div className="App">
+      Add Analysis
+    </div>
+  );
+}
+
+export default AddAnalysis;
